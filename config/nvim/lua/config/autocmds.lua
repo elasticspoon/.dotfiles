159dc712897ed24472e5a4f046286dc556fcceb4
Yuri Bocharov <quesadillaman@gmail.com>
2023-04-29 20:52:30 -0400

    window highlight

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -5,3 +5,20 @@ vim.api.nvim_create_autocmd("BufWritePost", {
 	pattern = "*.erb",
 	command = "!erblint -a --config ~/.erb-lint.yml %",
 })
+
+vim.api.nvim_create_autocmd({ "WinEnter", "FocusGained" }, {
+	callback = function()
+		vim.cmd("setlocal winhighlight=Normal:TelescopeResultsDiffDelete,NormalNC:Normal")
+		local timer = vim.loop.new_timer()
+		if timer == nil then
+			return
+		end
+		timer:start(
+			300,
+			0,
+			vim.schedule_wrap(function()
+				vim.cmd("setlocal winhighlight=Normal:Normal,NomralNC:NormalNC")
+			end)
+		)
+	end,
+})
