f729f6b7eac73a127f74081234a638bebfaaaf3d
Yuri Bocharov <quesadillaman@gmail.com>
2023-05-20 18:58:25 -0400

    added postcss autocommand

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -2,6 +2,7 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+local group_watcher = vim.api.nvim_create_augroup("FileWatch", {})
 
 -- TODO: make the next two autocmds check if the needed gems
 -- are installed before being created
@@ -9,6 +10,12 @@ vim.api.nvim_create_autocmd("BufWritePost", {
 	pattern = "*.erb",
 	group = augroup,
 	command = "!erblint -a --config ~/.erb-lint.yml %",
+})
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+	pattern = "*.postcss.css",
+	group = group_watcher,
+	command = "!yarn postcss ./app/assets/stylesheets/application.postcss.css -o ./app/assets/builds/application.css",
 })
 
 vim.api.nvim_create_autocmd("BufWritePost", {
