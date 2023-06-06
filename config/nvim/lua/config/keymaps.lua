4973ea50c6cd9019b921cb56efae4eac002de754
Yuri Bocharov <quesadillaman@gmail.com>
2023-06-06 18:45:44 -0400

    added stuff

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -7,3 +7,11 @@ vim.keymap.set("n", "<leader>bh", "<cmd>BufferLineCloseLeft<cr>", { desc = "Clos
 vim.keymap.set("n", "<leader>bl", "<cmd>BufferLineCloseRight<cr>", { desc = "Close Buffers to Right" })
 vim.keymap.del({ "n", "x", "o" }, "s")
 vim.keymap.del({ "n", "x", "o" }, "S")
+-- stylua: ignore
+vim.keymap.set("n", "<leader>tt", "<cmd>TestNearest -strategy=toggleterm<cr>", { desc = "Test Nearest" })
+vim.keymap.set("n", "<leader>tf", "<cmd>TestFile -strategy=toggleterm<cr>", { desc = "Test File" })
+vim.keymap.set("n", "<leader>tF", "<cmd>TestSuite -strategy=toggleterm<cr>", {
+	desc = "Test All Files",
+})
+vim.keymap.set("n", "<leader>tl", "<cmd>TestLast -strategy=toggleterm<cr>", { desc = "Run Last" })
+vim.keymap.set("n", "<leader>tv", "<cmd>TestVisit -strategy=toggleterm<cr>", { desc = "Visit Test File" })
--- config/nvim/lua/plugins/test.lua
+++ config/nvim/lua/plugins/test.lua
@@ -1,39 +1,14 @@
 return {
-	{ "antoinemadec/FixCursorHold.nvim" },
-	-- { "olimorris/neotest-rspec" },
-	-- { "zidhuss/neotest-minitest" },
-	-- { "elasticspoon/neotest-minitest" },
 	{ "vim-test/vim-test" },
-	{ "nvim-neotest/neotest-vim-test" },
+	{ "voldikss/vim-floaterm" },
 	{
-		"nvim-neotest/neotest",
+		"akinsho/toggleterm.nvim",
+		version = "*",
 		opts = {
-			-- Can be a list of adapters like what neotest expects,
-			-- or a list of adapter names,
-			-- or a table of adapter names, mapped to adapter configs.
-			-- The adapter will then be automatically loaded with the config.
-			adapters = {
-				-- ["neotest-minitest"] = {},
-				-- ["neotest-rspec"] = {},
-				["neotest-vim-test"] = {},
-			},
-			-- Example for loading neotest-go with a custom config
-			-- adapters = {
-			--   ["neotest-go"] = {
-			--     args = { "-tags=integration" },
-			--   },
-			-- },
-			status = { virtual_text = true },
-			output = { open_on_run = true },
-			quickfix = {
-				open = function()
-					if require("lazyvim.util").has("trouble.nvim") then
-						vim.cmd("Trouble quickfix")
-					else
-						vim.cmd("copen")
-					end
-				end,
-			},
+			direction = "vertical",
+			size = function()
+				return vim.o.columns * 0.3
+			end,
 		},
 	},
 }
