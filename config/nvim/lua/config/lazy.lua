ec3956a04b3d41f8f08abe95e7024d05cea02d2b
Yuri Bocharov <quesadillaman@gmail.com>
2023-06-14 17:25:54 -0400

    tailwind completion

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -15,6 +15,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.util.project" },
 		-- { import = "lazyvim.plugins.extras.test.core" },
 		{ import = "lazyvim.plugins.extras.ui.edgy" },
+		{ import = "lazyvim.plugins.extras.lang.tailwind" },
 		{ import = "lazyvim.plugins.extras.lang.typescript" },
 		{ import = "lazyvim.plugins.extras.linting.eslint" },
 		-- { import = "lazyvim.plugins.extras.dap.core" },
