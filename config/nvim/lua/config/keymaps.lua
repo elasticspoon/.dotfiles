249144924a1339cb8781b3512f951cb0a502a5ec
Yuri Bocharov <quesadillaman@gmail.com>
2023-06-23 14:19:42 -0400

    Split from "added neotest"

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -8,13 +8,3 @@ vim.keymap.set("n", "<leader>bl", "<cmd>BufferLineCloseRight<cr>", { desc = "Clo
 vim.keymap.del({ "n", "x", "o" }, "s")
 vim.keymap.del({ "n", "x", "o" }, "S")
 -- stylua: ignore
-vim.keymap.set("n", "<leader>tt", "<cmd>TestNearest<cr>", { desc = "Test Nearest" })
-vim.keymap.set("n", "<leader>tf", "<cmd>TestFile<cr>", { desc = "Test File" })
-vim.keymap.set("n", "<leader>tF", "<cmd>TestSuite<cr>", {
-	desc = "Test All Files",
-})
-vim.keymap.set("n", "<leader>tl", "<cmd>TestLast<cr>", { desc = "Run Last" })
-vim.keymap.set("n", "<leader>tv", "<cmd>TestVisit<cr>", { desc = "Visit Test File" })
-vim.keymap.set("n", "<leader>tk", "<cmd>VtrKillRunner<cr>", { desc = "Kill Runner Window" })
-vim.keymap.set("n", "<leader>to", "<cmd>VtrOpenRunner<cr>", { desc = "Open Runner" })
-vim.keymap.set("n", "<leader>tr", "<cmd>VtrFocusRunner<cr>", { desc = "Focus Runner" })
--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -13,13 +13,14 @@ require("lazy").setup({
 		-- import any extras modules here
 		{ import = "lazyvim.plugins.extras.coding.copilot" },
 		{ import = "lazyvim.plugins.extras.util.project" },
-		-- { import = "lazyvim.plugins.extras.test.core" },
+		{ import = "lazyvim.plugins.extras.test.core" },
+		{ import = "lazyvim.plugins.extras.lang.ruby" },
 		{ import = "lazyvim.plugins.extras.ui.edgy" },
 		{ import = "lazyvim.plugins.extras.lang.tailwind" },
 		{ import = "lazyvim.plugins.extras.lang.typescript" },
 		{ import = "lazyvim.plugins.extras.linting.eslint" },
 		{ import = "lazyvim.plugins.extras.formatting.prettier" },
-		-- { import = "lazyvim.plugins.extras.dap.core" },
+		{ import = "lazyvim.plugins.extras.dap.core" },
 		-- { import = "lazyvim.plugins.extras.lang.json" },
 		-- { import = "lazyvim.plugins.extras.ui.mini-animate" },
 		-- import/override with your plugins
