0269139e5bef3fb6fa7f70718d54de28ec8f673e
Yuri Bocharov <quesadillaman@gmail.com>
2023-06-25 22:00:36 -0400

    fixed primes stuff to fit me better

--- local/bin/tmux-cht.sh
+++ local/bin/tmux-cht.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-selected=$(cat ~/.tmux-cht-languages ~/.tmux-cht-command | fzf)
+selected=$(cat ~/.tmux/tmux-cht-languages ~/.tmux/tmux-cht-command | fzf)
 if [[ -z $selected ]]; then
 	exit 0
 fi
