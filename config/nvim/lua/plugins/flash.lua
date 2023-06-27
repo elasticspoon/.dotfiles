09a1d537fc859aa677befa23a251def8fca92965
Yuri Bocharov <quesadillaman@gmail.com>
2023-06-27 01:10:56 -0400

    more flash fixes

--- config/nvim/lua/plugins/flash.lua
+++ config/nvim/lua/plugins/flash.lua
@@ -6,110 +6,11 @@ return {
 				char = {
 					enabled = false,
 				},
+				search = {
+					enabled = false,
+				},
 			},
 		},
-		keys = {
-			{
-				"T",
-				mode = { "n", "x", "o" },
-				function()
-					require("flash").jump({
-						mode = "char",
-						search = {
-							forward = false,
-							mode = function(str)
-								return "\\V" .. str .. "\\zs\\m."
-							end,
-						},
-						jump = {
-							autojump = true,
-						},
-						highlight = {
-							matches = false,
-							label = {
-								style = "inline",
-								before = false,
-								after = { 0, 0 },
-							},
-						},
-					})
-				end,
-				desc = "Flash",
-			},
-			{
-				"t",
-				mode = { "n", "x", "o" },
-				function()
-					require("flash").jump({
-						mode = "char",
-						search = {
-							mode = function(str)
-								return "\\m.\\ze\\V" .. str
-							end,
-						},
-						jump = {
-							autojump = true,
-						},
-						highlight = {
-							matches = false,
-							label = {
-								style = "inline",
-								before = false,
-								after = { 0, 0 },
-							},
-						},
-					})
-				end,
-				desc = "Flash",
-			},
-			{
-				"F",
-				mode = { "n", "x", "o" },
-				function()
-					require("flash").jump({
-						mode = "char",
-						search = {
-							forward = false,
-						},
-						jump = {
-							autojump = true,
-						},
-						highlight = {
-							matches = false,
-							label = {
-								style = "inline",
-								before = false,
-								after = { 0, 0 },
-							},
-						},
-					})
-				end,
-				desc = "Flash",
-			},
-			{
-				"f",
-				mode = { "n", "x", "o" },
-				function()
-					require("flash").jump({
-						mode = "char",
-						search = {
-							forward = true,
-						},
-						jump = {
-							autojump = true,
-						},
-						highlight = {
-							matches = false,
-							label = {
-								style = "inline",
-								before = false,
-								after = { 0, 0 },
-							},
-						},
-					})
-				end,
-				desc = "Flash",
-			},
-		},
+		keys = {},
 	},
 }
