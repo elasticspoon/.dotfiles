04b5e3ba597287a2088bf1f0ae4d0fc0b9d137dd
Yuri Bocharov <quesadillaman@gmail.com>
2023-06-28 14:31:39 -0400

    fix for tmux split in vscode i3

--- config/i3/tmux-session.sh
+++ config/i3/tmux-session.sh
@@ -26,22 +26,21 @@ if [ "$TERMINAL_FOCUSED" = "true" ] && tmux has-session -t "$TMUX_SESSION_NAME" 
 	# check what process is running within the tmux session
 	TMUX_WINDOW_NAME=$(tmux display-message -p -t "$TMUX_SESSION_NAME" "#W")
 
-	# if [[ "$TMUX_WINDOW_NAME" == "nvim" ]]; then
-	# if [[ "$1" == "-h" ]]; then
-	# 	COMMAND=":vsp"
-	# else
-	# 	COMMAND=":sp"
-	# fi
-	# else
-	if [[ "$TMUX_WINDOW_NAME" != "nvim" ]]; then
+	if [[ "$TMUX_WINDOW_NAME" == "nvim" ]]; then
+		if [[ "$1" == "-h" ]]; then
+			COMMAND=":silent !tmux split-window -h"
+		else
+			COMMAND=":silent !tmux split-window -v"
+		fi
+	else
+		# if [[ "$TMUX_WINDOW_NAME" != "nvim" ]]; then
 		if [[ "$1" == "-h" ]]; then
 			COMMAND="tmux split-window -h"
 		else
 			COMMAND="tmux split-window -v"
 		fi
-		tmux send-keys -t "$TMUX_SESSION_NAME" "$COMMAND" 'Enter'
 	fi
-	# tmux send-keys -t "$TMUX_SESSION_NAME" "$COMMAND" Enter
+	tmux send-keys -t "$TMUX_SESSION_NAME" "$COMMAND" 'Enter'
 fi
 i3-msg "[con_id=$TERMINAL_ID] focus"
 
