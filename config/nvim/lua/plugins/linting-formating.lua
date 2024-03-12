228be70d19f85e0e4a71f657f8af70ca2c81a291
Yuri Bocharov <mail@yuribocharov.dev>
2024-03-11 22:45:59 -0400

    feat: git and nix

--- config/nvim/lua/plugins/linting-formating.lua
+++ config/nvim/lua/plugins/linting-formating.lua
@@ -4,7 +4,8 @@ return {
 		optional = true,
 		opts = {
 			formatters_by_ft = {
-				["eruby"] = { "erb_format", "htmlbeautifier" },
+				-- ["eruby"] = { "erb_format", "htmlbeautifier" },
+				["eruby"] = { "erb_format" },
 				-- ["javascript"] = { "prettier" },
 				["javascriptreact"] = { "prettier" },
 				-- ["typescript"] = { "prettier" },
