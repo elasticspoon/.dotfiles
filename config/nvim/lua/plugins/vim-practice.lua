c3c5ea8348ff37294f8bd945fde6bf860daf4770
elasticspoon <mail@yuribocharov.dev>
2024-06-04 15:25:39 -0400

    feat: improve vim prac

--- config/nvim/lua/plugins/vim-practice.lua
+++ config/nvim/lua/plugins/vim-practice.lua
@@ -19,20 +19,20 @@ return {
 		event = "VeryLazy",
 		config = {
 			-- startVisible = true,
-			-- showBlankVirtLine = true,
+			showBlankVirtLine = false,
 			-- highlightColor = "Comment",
-			-- hints = {
-			--      Caret = { text = "^", prio = 2 },
-			--      Dollar = { text = "$", prio = 1 },
-			--      MatchingPair = { text = "%", prio = 5 },
-			--      Zero = { text = "0", prio = 1 },
-			--      w = { text = "w", prio = 10 },
-			--      b = { text = "b", prio = 9 },
-			--      e = { text = "e", prio = 8 },
-			--      W = { text = "W", prio = 7 },
-			--      B = { text = "B", prio = 6 },
-			--      E = { text = "E", prio = 5 },
-			-- },
+			hints = {
+				Caret = { text = "^", prio = 0 },
+				Dollar = { text = "$", prio = 0 },
+				MatchingPair = { text = "%", prio = 0 },
+				Zero = { text = "0", prio = 0 },
+				w = { text = "w", prio = 0 },
+				b = { text = "b", prio = 0 },
+				e = { text = "e", prio = 0 },
+				W = { text = "W", prio = 0 },
+				B = { text = "B", prio = 0 },
+				E = { text = "E", prio = 0 },
+			},
 			-- gutterHints = {
 			--     -- prio is not currently used for gutter hints
 			--     G = { text = "G", prio = 1 },
