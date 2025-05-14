d573d7e0e2e6813c540390605ce058760cdea205
elasticspoon <mail@yuribocharov.dev>
2025-05-14 19:40:38 -0400

    fixup!

--- config/bash/scripts/setup/install-nvim.sh
+++ config/bash/scripts/setup/install-nvim.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod u+x nvim.appimage
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage
+chmod u+x nvim-linux-x86_64.appimage
 
 mkdir -p ~/.nvim
-mv ./nvim.appimage ~/.nvim/
-ln -s ~/.nvim/nvim.appimage ~/.local/bin/nvim
+mv ./nvim-linux-x86_64.appimage ~/.nvim/
+ln -sf ~/.nvim/nvim-linux-x86_64.appimage ~/.local/bin/nvim
