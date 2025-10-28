d113476d17fb4ae35ff55f3ae72feec5953cc3cf
elasticspoon <mail@yuribocharov.dev>
2025-10-28 11:08:50 -0400

    feat: nvim-cmp -> blink

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -15,7 +15,7 @@ opt.clipboard = "unnamedplus"
 opt.tags = { ".tags", ".git/tags", ".gemtags" }
 
 vim.g.snacks_animate = false
-vim.g.lazyvim_cmp = "nvim-cmp"
+-- vim.g.lazyvim_cmp = "nvim-cmp"
 -- LSP Server to use for Ruby.
 -- Set to "solargraph" to use solargraph instead of ruby_lsp.
 vim.g.lazyvim_ruby_lsp = "ruby_lsp"
