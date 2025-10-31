de77838185bcfb44164b0fdc24f8623b0ab9e503
Yuri Bocharov <yuri.bocharov@meraki.net>
2025-10-30 23:23:50 -0400

    fix: picker root dir issues

--- config/nvim/lua/plugins/fzf-lua.lua
+++ config/nvim/lua/plugins/fzf-lua.lua
@@ -3,6 +3,29 @@ return {
 		fzf_opts = {
 			["--layout"] = "default",
 		},
-	}
- },
+	} },
+	{
+		"folke/snacks.nvim",
+		opts = {
+			picker = {
+				enabled = false,
+			},
+		},
+		keys = {
+			{
+				"<leader><leader>",
+				function()
+					require("fzf-lua").files()
+				end,
+				desc = "Find Files",
+			},
+			{
+				"<leader>/",
+				function()
+					require("fzf-lua").live_grep_native()
+				end,
+				desc = "Grep (Root Dir)",
+			},
+		},
+	},
 }
